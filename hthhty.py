943097d2c34ebe9c76679d65839c3fda8ea8eac7
yastretel <44568527+yastretel@users.noreply.github.com>
2022-07-23 00:32:07 +0300

    Urok1

---
+++ hthhty.py
@@ -0,0 +1,4 @@
+name = input("Введите имя:")
+print(name)
+
+print("Привет", name)
---
+++ main.py
@@ -0,0 +1,25 @@
+strings = ["hello", "World"]
+numbers = [1, 2, 3, 4,5]
+data = ["hello", 1]
+
+print(strings)
+print(numbers)
+print(data)
+
+summa = numbers + data
+print(summa)
+
+numbers.append(6)
+print(numbers)
+
+first = strings[0]
+second = strings[1]
+print(first)
+print(second)
+
+strings_lenght = len(strings)
+print(strings_lenght)
+
+s = sum(numbers)
+print(s)
+
